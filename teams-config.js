80a5d93b153759ad494cc92776f0b4ef49aa75d3
whyfor666 <github@whyfor.com>
2025-06-19 14:29:34 -0400

    "Add placeholder images and logos"
    Added placeholder team HR images and team logos to /images and /logo folders.

--- teams-config.js
+++ teams-config.js
@@ -8,6 +8,10 @@ const teams = [
     { name: "Team 2",      color: "#ffff00", logo: "logo/team02-logo.png" },
     { name: "Team 3",      color: "#006400", logo: "logo/team03-logo.png" },
     { name: "Team 4",      color: "#ffffff", logo: "logo/team04-logo.png" },
-    { name: "team 5",      color: "#ffd700", logo: "logo/team05-logo.png" },
-    { name: "Team 6",      color: "#dc143c", logo: "logo/team06-logo.png" }
+    { name: "Team 5",      color: "#ffd700", logo: "logo/team05-logo.png" },
+    { name: "Team 6",      color: "#dc143c", logo: "logo/team06-logo.png" },
+    { name: "Team 7",      color: "#dc143c", logo: "logo/team07-logo.png" },
+    { name: "Team 8",      color: "#dc143c", logo: "logo/team08-logo.png" },
+    { name: "Team 9",      color: "#dc143c", logo: "logo/team09-logo.png" },
+    { name: "Team 10",      color: "#dc143c", logo: "logo/team10-logo.png" },
 ];
