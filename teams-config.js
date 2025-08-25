f717528010988692457ab338434909618ae66980
whyfor666 <github@whyfor.com>
2025-08-25 12:37:07 -0400

    Reformatting like hr-animations style
    Header app bar added
    14 default teams. 3x5 array

--- teams-config.js
+++ teams-config.js
@@ -2,16 +2,19 @@
 // Configuration for teams, their colors, and logos
 // Fields: team_name, team-background_color, team_logo_filename
 const teams = [
-    { name: "Away Team",   color: "#696969", logo: "logo/away-team-logo.png" },
-    { name: "Home Team",   color: "#ffffff", logo: "logo/home-team-logo.png" },
-    { name: "Team 1",      color: "#00bfff", logo: "logo/team01-logo.png" },
-    { name: "Team 2",      color: "#ffff00", logo: "logo/team02-logo.png" },
-    { name: "Team 3",      color: "#006400", logo: "logo/team03-logo.png" },
-    { name: "Team 4",      color: "#ffffff", logo: "logo/team04-logo.png" },
-    { name: "Team 5",      color: "#ffd700", logo: "logo/team05-logo.png" },
-    { name: "Team 6",      color: "#dc143c", logo: "logo/team06-logo.png" },
-    { name: "Team 7",      color: "#dc143c", logo: "logo/team07-logo.png" },
-    { name: "Team 8",      color: "#dc143c", logo: "logo/team08-logo.png" },
-    { name: "Team 9",      color: "#dc143c", logo: "logo/team09-logo.png" },
-    { name: "Team 10",      color: "#dc143c", logo: "logo/team10-logo.png" },
+    { name: "Away Team",           color: "#000000", logo: "logo/away-team.png" },  // Black			
+    { name: "Home Team",           color: "#ffffff", logo: "logo/home-team.png" },  // White			
+    { name: "OnlyFuns",            color: "#00bfff", logo: "logo/team01.png" },     // DeepSkyBlue	
+    { name: "Hollywoods",          color: "#00ff00", logo: "logo/team02.png" },     // LimeGreen		
+    { name: "Win Loose We Booze",  color: "#006400", logo: "logo/team03.png" },     // DarkGreen		
+    { name: "Suds Buds Windmills", color: "#ffffff", logo: "logo/team04.png" },     // White			
+    { name: "Batta Lions",         color: "#ffff00", logo: "logo/team05.png" },     // Yellow			
+    { name: "Diamond Dogs",        color: "#dc143c", logo: "logo/team06.png" },     // Crimson		
+    { name: "Velocity",            color: "#808080", logo: "logo/team07.png" },     // Gray			
+    { name: "Ice Cold Pitchers",   color: "#0000ff", logo: "logo/team08.png" },     // Blue			
+    { name: "Swingers",            color: "#000000", logo: "logo/team09.png" },     // Black			
+    { name: "Misfits",             color: "#00ced1", logo: "logo/team10.png" },     // DarkTurquoise	
+	{ name: "Jolly Taxpayers",     color: "#400080", logo: "logo/team11.png" },     // Purple			
+	{ name: "Green Death",         color: "#228b22", logo: "logo/team12.png" },     // ForestGreen	
+	{ name: "Fireballs",           color: "#ff4500", logo: "logo/team13.png" }      // OrangeRed	
 ];
